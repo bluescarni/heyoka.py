f80adfc456c0bece511ea853155ac431763a63db
Francesco Biscani <bluescarni@gmail.com>
2021-07-21 22:45:06 +0200

    Revert "asan attempt."
    This reverts commit 0c74f1bbca35450543bbeb2eeb7c43a510dbdfbb.

--- tools/travis_ubuntu_ppc64.sh
+++ tools/travis_ubuntu_ppc64.sh
@@ -7,14 +7,14 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential wget clang
+sudo apt-get install build-essential wget
 
 # Install conda+deps.
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-ppc64le.sh -O miniconda.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-ppc64le.sh -O miniconda.sh
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-mamba create -y -q -p $deps_dir cxx-compiler c-compiler cmake llvmdev boost-cpp sleef xtensor xtensor-blas blas blas-devel fmt spdlog python=3.8 pybind11 numpy mpmath sympy cloudpickle
+conda create -y -q -p $deps_dir cxx-compiler c-compiler cmake llvmdev boost-cpp sleef xtensor xtensor-blas blas blas-devel fmt spdlog python=3.8 pybind11 numpy mpmath sympy cloudpickle
 source activate $deps_dir
 
 # Checkout, build and install heyoka's HEAD.
@@ -23,10 +23,8 @@ cd heyoka_cpp
 mkdir build
 cd build
 
-export LDFLAGS="-shared-libasan ${LDFLAGS}"
-
 # GCC build.
-CC=clang CXX=clang++ cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_WITH_SLEEF=yes -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_CXX_FLAGS="-fsanitize=address -fno-common -U_FORTIFY_SOURCE"
+cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_WITH_SLEEF=yes -DBoost_NO_BOOST_CMAKE=ON
 make -j2 VERBOSE=1 install
 
 cd ../../
@@ -35,12 +33,12 @@ cd ../../
 mkdir build
 cd build
 
-CC=clang CXX=clang++ cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_PY_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_CXX_FLAGS="-fsanitize=address -fno-common -U_FORTIFY_SOURCE"
+cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_PY_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON
 make -j2 VERBOSE=1 install
 
 cd
 
-LD_PRELOAD=$(clang -print-file-name=libclang_rt.asan-powerpc64le.so) python -c "from heyoka import test; test.run_test_suite()"
+python -c "from heyoka import test; test.run_test_suite()"
 
 set +e
 set +x
