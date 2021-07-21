38f3d7e4f8ce241ae6259b6102825b1162b95554
Francesco Biscani <bluescarni@gmail.com>
2021-07-21 20:59:53 +0200

    Revert "gdb debugging attempt."
    This reverts commit ec06b625c6f74effdd74c39af727220e4de29c6a.

--- tools/travis_ubuntu_ppc64.sh
+++ tools/travis_ubuntu_ppc64.sh
@@ -7,7 +7,7 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential wget gdb
+sudo apt-get install build-essential wget
 
 # Install conda+deps.
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-ppc64le.sh -O miniconda.sh
@@ -38,11 +38,7 @@ make -j2 VERBOSE=1 install
 
 cd
 
-echo "run\nbt\nquit\n" > gdb_script
-
-gdb -x gdb_script  --args python -c "import heyoka; heyoka.test.run_test_suite();"
-
-#python -c "from heyoka import test; test.run_test_suite()"
+python -c "from heyoka import test; test.run_test_suite()"
 
 set +e
 set +x
