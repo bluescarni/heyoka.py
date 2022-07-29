8b16a049672b04e5ca086cfa6896d12d9318faf6
Francesco Biscani <bluescarni@gmail.com>
2022-07-29 20:07:42 +0200

    Bump up fmt and spdlog deps in the CI.

--- tools/circleci_ubuntu_arm64.sh
+++ tools/circleci_ubuntu_arm64.sh
@@ -14,7 +14,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-conda create -y -q -p $deps_dir cxx-compiler c-compiler cmake llvmdev tbb-devel tbb astroquery boost-cpp sleef 'fmt=8.0.*' 'spdlog=1.9.2' python=3.8 pybind11 numpy mpmath sympy scipy cloudpickle sphinx 'myst-nb<0.14' matplotlib sphinx-book-theme
+conda create -y -q -p $deps_dir cxx-compiler c-compiler cmake llvmdev tbb-devel tbb astroquery boost-cpp sleef 'fmt=8.1.*' 'spdlog=1.10.*' python=3.8 pybind11 numpy mpmath sympy scipy cloudpickle sphinx 'myst-nb<0.14' matplotlib sphinx-book-theme
 source activate $deps_dir
 
 export HEYOKA_PY_PROJECT_DIR=`pwd`
