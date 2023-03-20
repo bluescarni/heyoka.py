0e961cc7e569ce5a6373aaeef9526d0c794b72ae
Francesco Biscani <bluescarni@gmail.com>
2023-03-20 09:49:58 +0100

    Some initial work on fixed centres exposition, simplify internals.

--- heyoka/expose_models.cpp
+++ heyoka/expose_models.cpp
@@ -8,14 +8,20 @@
 
 #include <heyoka/config.hpp>
 
-#include <heyoka/model/pendulum.hpp>
+#include <algorithm>
+#include <iterator>
 #include <optional>
 #include <string>
 #include <variant>
 #include <vector>
 
+#include <fmt/core.h>
+
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+
+#include <Python.h>
 
 #if defined(HEYOKA_HAVE_REAL128)
 
@@ -32,7 +38,9 @@
 #include <heyoka/expression.hpp>
 #include <heyoka/models.hpp>
 
+#include "common_utils.hpp"
 #include "custom_casters.hpp"
+#include "dtypes.hpp"
 #include "expose_models.hpp"
 
 namespace heyoka_py
@@ -44,8 +52,9 @@ namespace detail
 namespace
 {
 
-// Small helper to turn v into an expression.
-constexpr auto make_ex = [](const auto &v) { return heyoka::expression(v); };
+// Small helper to turn the variant v into an expression.
+constexpr auto ex_from_variant
+    = [](const auto &v) { return std::visit([](const auto &v) { return heyoka::expression(v); }, v); };
 
 // Common logic to expose N-body helpers.
 template <typename Op, typename V>
@@ -53,13 +62,11 @@ auto nbody_impl(const Op &op, std::uint32_t n, const V &Gconst, const std::optio
 {
     namespace hy = heyoka;
 
-    const auto Gval = std::visit(make_ex, Gconst);
+    const auto Gval = ex_from_variant(Gconst);
 
     if (masses) {
         std::vector<hy::expression> masses_vec;
-        for (const auto &mass : *masses) {
-            masses_vec.push_back(std::visit(make_ex, mass));
-        }
+        std::transform(masses->begin(), masses->end(), std::back_inserter(masses_vec), ex_from_variant);
 
         return op(n, hy::kw::Gconst = Gval, hy::kw::masses = masses_vec);
     } else {
@@ -73,10 +80,51 @@ auto pendulum_impl(const Op &op, const V &gconst, const V &l)
 {
     namespace hy = heyoka;
 
-    const auto gval = std::visit(make_ex, gconst);
-    const auto lval = std::visit(make_ex, l);
+    const auto gval = ex_from_variant(gconst);
+    const auto lval = ex_from_variant(l);
 
     return op(hy::kw::gconst = gval, hy::kw::l = lval);
+}
+
+// Common logic to expose fixed centres helpers.
+template <typename Op, typename V>
+auto fixed_centres_impl(const Op &op, const V &Gconst, const std::vector<V> &masses, const py::array &positions)
+{
+    namespace hy = heyoka;
+
+    // Check the shape of positions.
+    if (positions.ndim() != 2) {
+        py_throw(PyExc_ValueError, fmt::format("Invalid positions array in a fixed centres model: the number of "
+                                               "dimensions must be 2, but it is {} instead",
+                                               positions.ndim())
+                                       .c_str());
+    }
+
+    if (positions.shape(1) != 3) {
+        py_throw(PyExc_ValueError, fmt::format("Invalid positions array in a fixed centres model: the number of "
+                                               "columns must be 3, but it is {} instead",
+                                               positions.shape(1))
+                                       .c_str());
+    }
+
+    // Flatten out postions into a list and convert to a vector of variants.
+    py::list pflat = positions.attr("flatten")();
+    std::vector<V> pvec;
+    try {
+        pvec = pflat.cast<std::vector<V>>();
+    } catch (const py::cast_error &) {
+        py_throw(PyExc_TypeError,
+                 "The positions array in a fixed centres model could not be converted into an array "
+                 "of expressions - please make sure that the array's values can be converted into heyoka expressions");
+    }
+
+    // Build the arguments for the C++ function.
+    const auto Gval = ex_from_variant(Gconst);
+    std::vector<hy::expression> masses_vec, positions_vec;
+    std::transform(masses.begin(), masses.end(), std::back_inserter(masses_vec), ex_from_variant);
+    std::transform(pvec.begin(), pvec.end(), std::back_inserter(positions_vec), ex_from_variant);
+
+    return op(hy::kw::Gconst = Gval, hy::kw::masses = masses_vec, hy::kw::positions = positions_vec);
 }
 
 } // namespace
@@ -118,6 +166,12 @@ void expose_models(py::module_ &m)
         },
         "n"_a.noconvert(), "Gconst"_a.noconvert() = 1., "masses"_a.noconvert() = py::none{});
     m.def(
+        "_model_nbody_potential",
+        [](std::uint32_t n, const vex_t &Gconst, const std::optional<std::vector<vex_t>> &masses) {
+            return detail::nbody_impl(hy::model::nbody_potential, n, Gconst, masses);
+        },
+        "n"_a.noconvert(), "Gconst"_a.noconvert() = 1., "masses"_a.noconvert() = py::none{});
+    m.def(
         "_model_np1body",
         [](std::uint32_t n, const vex_t &Gconst, const std::optional<std::vector<vex_t>> &masses) {
             return detail::nbody_impl(hy::model::np1body, n, Gconst, masses);
@@ -127,6 +181,12 @@ void expose_models(py::module_ &m)
         "_model_np1body_energy",
         [](std::uint32_t n, const vex_t &Gconst, const std::optional<std::vector<vex_t>> &masses) {
             return detail::nbody_impl(hy::model::np1body_energy, n, Gconst, masses);
+        },
+        "n"_a.noconvert(), "Gconst"_a.noconvert() = 1., "masses"_a.noconvert() = py::none{});
+    m.def(
+        "_model_np1body_potential",
+        [](std::uint32_t n, const vex_t &Gconst, const std::optional<std::vector<vex_t>> &masses) {
+            return detail::nbody_impl(hy::model::np1body_potential, n, Gconst, masses);
         },
         "n"_a.noconvert(), "Gconst"_a.noconvert() = 1., "masses"_a.noconvert() = py::none{});
 
@@ -141,6 +201,29 @@ void expose_models(py::module_ &m)
             return detail::pendulum_impl(hy::model::pendulum_energy, gconst, l);
         },
         "gconst"_a.noconvert() = 1., "l"_a.noconvert() = 1.);
+
+    // Fixed centres.
+    m.def(
+        "_model_fixed_centres",
+        [](const vex_t &Gconst, const std::vector<vex_t> &masses, const py::array &positions) {
+            return detail::fixed_centres_impl(hy::model::fixed_centres, Gconst, masses, positions);
+        },
+        "Gconst"_a.noconvert() = 1., "masses"_a.noconvert() = py::list{},
+        "positions"_a = py::array{py::dtype(get_dtype<double>()), py::array::ShapeContainer{0, 3}});
+    m.def(
+        "_model_fixed_centres_energy",
+        [](const vex_t &Gconst, const std::vector<vex_t> &masses, const py::array &positions) {
+            return detail::fixed_centres_impl(hy::model::fixed_centres_energy, Gconst, masses, positions);
+        },
+        "Gconst"_a.noconvert() = 1., "masses"_a.noconvert() = py::list{},
+        "positions"_a = py::array{py::dtype(get_dtype<double>()), py::array::ShapeContainer{0, 3}});
+    m.def(
+        "_model_fixed_centres_potential",
+        [](const vex_t &Gconst, const std::vector<vex_t> &masses, const py::array &positions) {
+            return detail::fixed_centres_impl(hy::model::fixed_centres_potential, Gconst, masses, positions);
+        },
+        "Gconst"_a.noconvert() = 1., "masses"_a.noconvert() = py::list{},
+        "positions"_a = py::array{py::dtype(get_dtype<double>()), py::array::ShapeContainer{0, 3}});
 }
 
 } // namespace heyoka_py
