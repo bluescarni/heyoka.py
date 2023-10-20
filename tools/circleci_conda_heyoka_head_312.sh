cec385a73dc91fe61fa7473e649a630e297127ab
Francesco Biscani <bluescarni@gmail.com>
2023-10-20 08:32:37 +0200

    Add Python 3.12 build.

---
+++ tools/circleci_conda_heyoka_head_312.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install build-essential wget
+
+# Install conda+deps.
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda create -y -q -p $deps_dir python=3.12 git pybind11 numpy mpmath cmake llvmdev tbb-devel tbb astroquery boost-cpp mppp sleef fmt spdlog sphinx myst-nb matplotlib sympy scipy pykep cloudpickle sphinx-book-theme
+source activate $deps_dir
+
+export HEYOKA_PY_PROJECT_DIR=`pwd`
+
+# Checkout, build and install heyoka's HEAD.
+git clone https://github.com/bluescarni/heyoka.git heyoka_cpp
+cd heyoka_cpp
+mkdir build
+cd build
+
+cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_WITH_MPPP=yes -DHEYOKA_WITH_SLEEF=yes -DBoost_NO_BOOST_CMAKE=ON
+make -j2 VERBOSE=1 install
+
+cd ../../
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_PY_ENABLE_IPO=yes -DBoost_NO_BOOST_CMAKE=ON
+make -j2 VERBOSE=1 install
+
+cd ../tools
+
+python ci_test_runner.py
+
+cd $HEYOKA_PY_PROJECT_DIR
+
+cd doc
+
+make html linkcheck
+
+set +e
+set +x
