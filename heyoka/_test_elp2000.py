113fc65a9f772902e5a0219eaf025387f18cb1c5
Francesco Biscani <bluescarni@gmail.com>
2023-11-20 16:47:21 +0100

    Bits of testing for elp2000 and vsop2013.

---
+++ heyoka/_test_elp2000.py
@@ -0,0 +1,28 @@
+# Copyright 2020, 2021, 2022, 2023 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+#
+# This file is part of the heyoka.py library.
+#
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+import unittest as _ut
+
+
+class elp2000_test_case(_ut.TestCase):
+    # Just a small basic test.
+    def test_basic(self):
+        from . import make_cfunc
+        from .model import elp2000_cartesian_e2000, elp2000_cartesian_fk5
+
+        sol = elp2000_cartesian_e2000(thresh=1e-5)[0]
+        cf = make_cfunc([sol])
+
+        date = 2469000.5
+        self.assertAlmostEqual(cf([], time=(date - 2451545.0) / 36525)[0], -361605.79234692274)
+
+        sol = elp2000_cartesian_fk5(thresh=1e-5)[0]
+        cf = make_cfunc([sol])
+
+        self.assertAlmostEqual(cf([], time=(date - 2451545.0) / 36525)[0], -361605.7668217605)
---
+++ heyoka/_test_vsop2013.py
@@ -0,0 +1,28 @@
+# Copyright 2020, 2021, 2022, 2023 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+#
+# This file is part of the heyoka.py library.
+#
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+import unittest as _ut
+
+
+class vsop2013_test_case(_ut.TestCase):
+    # Just a small basic test.
+    def test_basic(self):
+        from . import make_cfunc
+        from .model import vsop2013_elliptic, vsop2013_cartesian
+
+        sol = vsop2013_elliptic(1, 1)
+        cf = make_cfunc([sol])
+
+        date = 2411545.0
+        self.assertAlmostEqual(cf([], time=(date - 2451545.0) / 365250)[0], 0.3870979635)
+
+        sol = vsop2013_cartesian(1, thresh=1e-8)
+        cf = make_cfunc([sol[0]])
+
+        self.assertAlmostEqual(cf([], time=(date - 2451545.0) / 365250)[0], 0.3493879042)
