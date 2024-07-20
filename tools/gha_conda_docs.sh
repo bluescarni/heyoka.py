8da3b6d8b4b464ff40c752ce805b6b8310ed8811
Francesco Biscani <bluescarni@gmail.com>
2024-07-20 13:49:24 +0200

    Fix doc deploy.

--- tools/gha_conda_docs.sh
+++ tools/gha_conda_docs.sh
@@ -14,9 +14,12 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniforge/bin:$PATH"
 bash miniforge.sh -b -p $HOME/miniforge
+# NOTE: the scipy pin is necessary otherwise the
+# notebooks do not execute correctly due to pykep
+# using deprecated scipy functions.
 mamba create -y -p $deps_dir c-compiler cxx-compiler python=3.10 git pybind11 \
     ninja 'numpy<2' mpmath cmake llvmdev tbb-devel tbb astroquery libboost-devel \
-    'mppp=1.*' sleef 'fmt<11' skyfield spdlog myst-nb matplotlib sympy scipy pykep cloudpickle \
+    'mppp=1.*' sleef 'fmt<11' skyfield spdlog myst-nb matplotlib sympy 'scipy<1.14' pykep cloudpickle \
     'sphinx=7.*' 'sphinx-book-theme=1.*'
 source activate $deps_dir
 
