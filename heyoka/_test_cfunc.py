a4903d6d042810e2d4a30467fc6f7f86bbe23408
Francesco Biscani <bluescarni@gmail.com>
2024-08-31 14:22:25 +0200

    Some testing for the new parjit and code_model options.

--- heyoka/_test_cfunc.py
+++ heyoka/_test_cfunc.py
@@ -12,14 +12,21 @@ import unittest as _ut
 
 class cfunc_test_case(_ut.TestCase):
     def test_basic(self):
-        from . import cfunc, make_vars, cfunc_dbl, core, par, time
+        from . import cfunc, make_vars, cfunc_dbl, core, par, time, code_model
         import pickle
         from copy import copy, deepcopy
+        from sys import getrefcount
 
         self.assertRaises(ValueError, lambda: cfunc([], []))
 
         x, y, z, s = make_vars("x", "y", "z", "s")
         cf = cfunc([y * (x + z)], [x, y, z])
+
+        # Ensure that when extracting the llvm states
+        # the ref count of cf is properly updated.
+        rc = getrefcount(cf)
+        tmp = cf.llvm_states
+        self.assertEqual(getrefcount(cf), rc + 3)
 
         self.assertTrue(all(not _.force_avx512 for _ in cf.llvm_states))
         self.assertTrue(all(not _.slp_vectorize for _ in cf.llvm_states))
@@ -47,11 +54,26 @@ class cfunc_test_case(_ut.TestCase):
             [_.get_ir() for _ in cf.llvm_states],
         )
 
-        cf = cfunc([y * (x + z)], vars=[y, z, x], force_avx512=True, slp_vectorize=True)
-        self.assertEqual(cf.vars, [y, z, x])
+        # Construct with custom LLVM settings too.
+        cf = cfunc(
+            [y * (x + z)],
+            vars=[y, z, x],
+            force_avx512=True,
+            slp_vectorize=True,
+            compact_mode=True,
+            parjit=False,
+            code_model=code_model.large,
+        )
 
-        self.assertTrue(all(_.force_avx512 for _ in cf.llvm_states))
-        self.assertTrue(all(_.slp_vectorize for _ in cf.llvm_states))
+        rc = getrefcount(cf)
+        tmp = cf.llvm_states
+        self.assertEqual(getrefcount(cf), rc + 1)
+
+        self.assertEqual(cf.vars, [y, z, x])
+        self.assertTrue(cf.llvm_states.force_avx512)
+        self.assertTrue(cf.llvm_states.slp_vectorize)
+        self.assertFalse(cf.llvm_states.parjit)
+        self.assertEqual(cf.llvm_states.code_model, code_model.large)
 
         # Tests for correct detection of number of params, time dependency
         # and list of variables.
