f47b0f27fe63c9a3ccbe35a796651d9f8dfea3ed
Francesco Biscani <bluescarni@gmail.com>
2024-12-17 08:18:42 +0100

    Again.

--- tools/gha_osx_heyoka_head_static.sh
+++ tools/gha_osx_heyoka_head_static.sh
@@ -12,9 +12,13 @@ export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda create -y -p $deps_dir python=3.11 c-compiler cxx-compiler git pybind11 'numpy>=2' \
-    cmake llvmdev tbb-devel tbb astroquery libboost-devel sleef fmt skyfield \
-    spdlog sympy cloudpickle 'mppp=2.*' numba 'clang<19' 'clangxx<19'
+    ninja cmake llvmdev tbb-devel tbb astroquery libboost-devel sleef fmt skyfield \
+    spdlog sympy cloudpickle 'mppp=2.*' numba
 source activate $deps_dir
+
+# Clear the compilation flags set up by conda.
+unset CXXFLAGS
+unset CFLAGS
 
 # Checkout, build and install heyoka's HEAD.
 git clone --depth 1 https://github.com/bluescarni/heyoka.git heyoka_cpp
@@ -22,8 +26,17 @@ cd heyoka_cpp
 mkdir build
 cd build
 
-cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_WITH_SLEEF=yes -DHEYOKA_WITH_MPPP=yes -DHEYOKA_FORCE_STATIC_LLVM=yes -DHEYOKA_HIDE_LLVM_SYMBOLS=yes
-make -j2 VERBOSE=1 install
+cmake ../ -G Ninja \
+    -DCMAKE_INSTALL_PREFIX=$deps_dir \
+    -DCMAKE_PREFIX_PATH=$deps_dir \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DHEYOKA_WITH_SLEEF=yes \
+    -DHEYOKA_WITH_MPPP=yes \
+    -DHEYOKA_FORCE_STATIC_LLVM=yes \
+    -DHEYOKA_HIDE_LLVM_SYMBOLS=yes \
+    -DCMAKE_CXX_FLAGS_DEBUG="-g -Og"
+
+ninja -j2 -v install
 
 cd ../../
 
@@ -31,8 +44,13 @@ cd ../../
 mkdir build
 cd build
 
-cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DHEYOKA_PY_ENABLE_IPO=yes
-make -j2 VERBOSE=1 install
+cmake ../ -G Ninja \
+    -DCMAKE_INSTALL_PREFIX=$deps_dir \
+    -DCMAKE_PREFIX_PATH=$deps_dir \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_CXX_FLAGS_DEBUG="-g -Og"
+
+ninja -j2 -v install
 
 cd ../tools
 
