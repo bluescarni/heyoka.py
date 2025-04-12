31a545a72d5074a6d2dc90ef2152dceffddd6132
Francesco Biscani <bluescarni@gmail.com>
2025-04-12 11:46:48 +0200

    Add cartopy dep when building the docs.

--- tools/conda_docs.sh
+++ tools/conda_docs.sh
@@ -16,7 +16,7 @@ export PATH="$HOME/miniforge/bin:$PATH"
 bash miniforge.sh -b -p $HOME/miniforge
 conda create -y -p $deps_dir c-compiler cxx-compiler python=${HEYOKA_PY_PY_VERSION} git pybind11 \
     ninja 'numpy>=2' mpmath cmake llvmdev tbb-devel tbb astroquery libboost-devel \
-    'mppp=2.*' sleef fmt skyfield spdlog myst-nb matplotlib sympy scipy cloudpickle \
+    'mppp=2.*' sleef fmt skyfield spdlog myst-nb matplotlib sympy scipy cartopy cloudpickle \
     'sphinx=8.*' 'sphinx-book-theme=1.*'
 source activate $deps_dir
 
