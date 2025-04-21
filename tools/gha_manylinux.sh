1ff59233280c5f417918acd4d4360179f7012961
Francesco Biscani <bluescarni@gmail.com>
2025-04-21 21:42:20 +0200

    Try with uv.

--- tools/gha_manylinux.sh
+++ tools/gha_manylinux.sh
@@ -36,8 +36,8 @@ fi
 # Report the inferred directory where python is found.
 echo "PYTHON_DIR: ${PYTHON_DIR}"
 
-yum install -y libffi-devel
-/opt/python/${PYTHON_DIR}/bin/pip install twine
+# Install uv.
+/opt/python/${PYTHON_DIR}/bin/pip install uv
 
 # The heyoka version to be used for releases.
 export HEYOKA_VERSION_RELEASE="7.3.0"
@@ -97,7 +97,7 @@ if [[ "${HEYOKA_PY_BUILD_SDIST}" == "yes" ]]; then
 	fi
 else
 	# Build the heyoka.py wheel.
-	/opt/python/${PYTHON_DIR}/bin/pip wheel . -v
+	/opt/python/${PYTHON_DIR}/bin/uv build
 	# Repair it.
 	auditwheel repair ./heyoka*.whl -w ./repaired_wheel
 	# Report the size.
@@ -112,7 +112,7 @@ else
 
 	# Upload to PyPI.
 	if [[ "${HEYOKA_PY_RELEASE_BUILD}" == "yes" ]]; then
-		/opt/python/${PYTHON_DIR}/bin/twine upload -u __token__ ${GITHUB_WORKSPACE}/repaired_wheel/heyoka*
+		/opt/python/${PYTHON_DIR}/bin/uv publish -u __token__ ${GITHUB_WORKSPACE}/repaired_wheel/heyoka*
 	fi
 fi
 
