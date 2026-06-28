23b70cba5a669062b1ce6318fb1f0e04de05f2ab
Francesco Biscani <bluescarni@gmail.com>
2026-06-28 12:09:57 +0200

    More WIP.

--- heyoka/callback/__init__.py
+++ heyoka/callback/__init__.py
@@ -6,13 +6,6 @@
 # Public License v. 2.0. If a copy of the MPL was not distributed
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from .. import core as _core
+from .._core import angle_reducer
 
-_lst = list(filter(lambda name: name.startswith("_callback_"), dir(_core)))
-
-for _name in _lst:
-    exec(f"from ..core import {_name} as {_name[10:]}")
-
-del _core
-del _lst
-del _name
+__all__ = ["angle_reducer"]
